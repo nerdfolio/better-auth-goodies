a3b8d1f2b2d7b7e73b470b223f29f99155fe11c6
Tai Vo <tai.a.vo@gmail.com>
2025-06-18 15:34:51 -0700

    init workspace

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { type Options, defineConfig } from "tsup"
+
+const libCfg = {
+	entry: ["src/index.ts"],
+	splitting: false,
+	target: "node22",
+	format: ["esm", "cjs"],
+	dts: true,
+	clean: true,
+	outExtension({ format }) {
+		return { js: format === "esm" ? ".mjs" : format === "cjs" ? ".cjs" : ".js" }
+	},
+} satisfies Options
+
+export default defineConfig(libCfg)
